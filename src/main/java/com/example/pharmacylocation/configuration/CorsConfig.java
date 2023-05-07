944e8ab2c6cb73c4f8f3421a4ff275b8645bb663
youssefelyou <youssefelyourizi36@gmail.com>
2023-05-07 23:37:36 +0100

    modif

---
+++ src/main/java/com/example/pharmacylocation/configuration/CorsConfig.java
@@ -0,0 +1,4 @@
+package com.example.pharmacylocation.configuration;
+
+public class CorsConfig {
+}
--- src/main/java/com/example/pharmacylocation/controller/VilleController.java
+++ src/main/java/com/example/pharmacylocation/controller/VilleController.java
@@ -11,6 +11,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("api/villes")
+@CrossOrigin
 public class VilleController {
 
     @Autowired
