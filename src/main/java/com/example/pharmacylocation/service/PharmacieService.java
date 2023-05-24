08d9d59b2baa08390be1939823c81927bedcdc81
youssefelyou <youssefelyourizi36@gmail.com>
2023-05-24 23:52:39 +0100

    Pharmacie updated

--- src/main/java/com/example/pharmacylocation/service/PharmacieService.java
+++ src/main/java/com/example/pharmacylocation/service/PharmacieService.java
@@ -4,6 +4,7 @@ package com.example.pharmacylocation.service;
 import com.example.pharmacylocation.bean.Pharmacie;
 import com.example.pharmacylocation.repository.PharmacieRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -30,5 +31,16 @@ public class PharmacieService {
 
     public List<Pharmacie> findAll() {
         return pharmacieRepository.findAll();
+    }
+
+    public void update(Integer id,Pharmacie pharmacieinfo) {
+        Pharmacie pharmacie=pharmacieRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("pharmacie not found with id " + id));
+        pharmacie.setAddresse(pharmacieinfo.getAddresse());
+        pharmacie.setNom(pharmacieinfo.getNom());
+        pharmacie.setZone(pharmacieinfo.getZone());
+        pharmacie.setLatitude(pharmacieinfo.getLatitude());
+        pharmacie.setLongitude(pharmacieinfo.getLongitude());
+        pharmacie.setPhoto(pharmacieinfo.getPhoto());
+        pharmacieRepository.save(pharmacie);
     }
 }
